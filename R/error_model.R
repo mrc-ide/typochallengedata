58b0475beb48ba0a091c09c5e9b985277727aab9
Marc Baguelin <mbagueli@wpia-dide280.dide.ic.ac.uk>
2020-01-08 11:45:38 +0000

    all error model matrix apart from distant substitution

--- R/error_model.R
+++ R/error_model.R
@@ -65,7 +65,7 @@ generate_neighbour_substitution <- function(date_d, kb_type = "upper")
   
   for(p in pos){
     res <- c(res,generate_neighbour_substitution_one_digit_as_char(date_d=date_d,position=p,kb_type=kb_type))
-    browser()
+    #browser()
   }
   return(as.Date(res,format="%d/%m/%Y"))
 }
@@ -75,8 +75,10 @@ calculate_date_matrix <- function(start_date, end_date, p_error, format_date)
   #sequence of potential dates
   potential_dates <- seq(from=start_date, to=end_date, by=1)
   
-  length_period <- as.integer(end_date - start_date)
+  #calculate the number of days in the date space
+  length_period <- length(potential_dates)
   
+  #initialise the matrix with the random given probabilities divided by the size of the date space
   likelihood_error_date <- matrix(rep(0,length_period^2),ncol=length_period)
   
   for(d in 1:length_period)
@@ -84,22 +86,30 @@ calculate_date_matrix <- function(start_date, end_date, p_error, format_date)
     #browse through the date entries
     date_d <- potential_dates[d]
     
-    #generate the potential external swap error
+    ####################### external swap #######################
+    
+    #generates the potential external swap error dates
     es_date <- generate_external_swap(date_d)
     
-    if(!is.na(es_date))
-    {
-      es_date_pos <- which(es_date==potential_dates)
-      #browser()
-      likelihood_error_date[d,es_date_pos] <- likelihood_error_date[d,es_date_pos] + p_error$external_swap
-    }
+    #generates and adds the probabilities in the likelihood matrix
+    likelihood_error_date[,d] <- likelihood_error_date[,d] + generate_vector_prob_dates(date_set = es_date, date_space = date_d, prob=p_error$external_swap)
     
-    #internal swap
+    ####################### internal swap #######################
     
+    #generates the potential external swap error dates
+    is_date <- generate_internal_swap(date_d)
     
+    #generates and adds the probabilities in the likelihood matrix
+    likelihood_error_date[,d] <- likelihood_error_date[,d] + generate_vector_prob_dates(date_set = es_date, date_space = date_d, prob=p_error$internal_swap)
     
+    ####################### neigbour substitution #######################
     
-    #likelihood_error_date[d,es_date_pos]
+    #generates the potential external swap error dates
+    ns_date <- generate_neighbour_substitution(date_d)
+    
+    #generates and adds the probabilities in the likelihood matrix
+    likelihood_error_date[,d] <- likelihood_error_date[,d] + generate_vector_prob_dates(date_set = ns_date, date_space = date_d, prob=p_error$neighbour_substitution)
+    
     
   }
   
@@ -107,12 +117,37 @@ calculate_date_matrix <- function(start_date, end_date, p_error, format_date)
   
 }
 
-list_errors <- list(external_swap=.04,internal_swap=.005,neighbour_mutation=0.01,distant_mutation=0.02,random=0.01)
+list_errors <- list(external_swap=.04,internal_swap=.005,neighbour_substitution=0.01,distant_substitution=0.02,random=0.01)
 
 #take the dates from date_set (potentially NA) and return a vector mapping the date_space vector with probabilities
-#such that the the elements in the date_set (including each NA) have the same probabilities summing to prob
+#such that the the elements in the date_set (including each NA) have the same probabilities summing to
 generate_vector_prob_dates <- function(date_set, date_space, prob){
   
+  #Count the number of element in the set
+  n_d_set <- length(date_set)
+  
+  #test if the elements from the set belong to the date space
+  present_in_d_space <- (date_set %in% date_space)
+  
+  #for each element of the set present in the space, return their respective position in the date space
+  v <- sapply(date_set[present_in_d_space], FUN = function(x){return(which(date_space==x))})
+  
+  #for each element in the date space, produces how many times it is present in the date_set
+  result <- tabulate(v, nbins = length(date_space))
+  
+  #return the probability increment for the error matrix
+  #given by the frequency in the set (v), the overall probability (prob) divided by the number of elements (n_d_set)
+  return(result*prob/n_d_set)
 }
+
+# d_s <- seq(from =as.Date("28/12/2019", "%d/%m/%Y"),length.out = 365, by = 1)
+# 
+# s_to_test <- generate_neighbour_substitution(as.Date("03/02/2020", "%d/%m/%Y"), kb_type = "upper")
+# 
+# res_v <- generate_vector_prob_dates(date_set = s_to_test, date_space = d_s, prob=0.05)
+# 
+# names(res_v) <- d_s
+# 
+# res_v[res_v>0]
 
 #mat_e_d <- calculate_date_matrix(start_date=as.Date("28/12/2019", "%d/%m/%Y"),end_date=as.Date("28/12/2020", "%d/%m/%Y"), p_error=list_errors)
